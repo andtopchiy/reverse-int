6459fa8b489ef78194ee3efc603f5613bb1c67eb
AndriiTopchii <3988359+andtopchiy@users.noreply.github.com>
2020-03-13 09:18:37 +0200

    fix: add solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let notStr = String(Math.abs(n));
+    let result = '';
+
+    for (let i=notStr.length-1; i>=0; i--) {
+        result+=notStr[i];
+    }
+
+    return parseInt(result);
 }
